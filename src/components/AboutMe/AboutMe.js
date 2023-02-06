acfa01a7e7c7178a9f7064f16762278dc89b0247
Louis Delia <delia.louis@gmail.com>
2023-02-05 22:13:21 -0500

    updating about-me section selectors and style

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -6,7 +6,7 @@ export default function AboutMe() {
     <>
       <Container fixed>
         <Box>
-          <div className="about-me">
+          <div id="about-me">
             <h2>About Me</h2>
             <Avatar
               className="about-me-avatar"
@@ -14,7 +14,7 @@ export default function AboutMe() {
               src="images/louisdelia.jpg"
               sx={{ width: 250, height: 250, margin: "20px auto" }}
             />
-            <p>
+            <p className="about-me-description">
               Louis T. Delia (he/they) is a web developer, musician, and
               blogger. He possesses six years of experience as a software developer, spanning both medical communications and telecommunications. He has worked heavily in JavaScript and TypeScript, utilizing his skills to work on a range of static websites, touchscreen kiosks, progressive web applications, and software-as-a-service (Saas) products. He is currently seeking work, and is open to opportunities. In his spare time, he enjoys writing music.
             </p>
