4500023fbb87d1ee03ee7edbc4ea45b8db6f80e0
Louis Delia <delia.louis@gmail.com>
2023-02-05 20:28:56 -0500

    styling about me

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -8,7 +8,12 @@ export default function AboutMe() {
         <Box>
           <div className="about-me">
             <h2>About Me</h2>
-            <Avatar alt="Louis T. Delia" src="images/louisdelia.jpg" sx={{ width: 250, height: 250 }} />
+            <Avatar
+              className="about-me-avatar"
+              alt="Louis T. Delia"
+              src="images/louisdelia.jpg"
+              sx={{ width: 250, height: 250, margin: "20px auto" }}
+            />
             <p>
               Louis T. Delia (he/they) is a web developer, musician, and
               blogger. He is currently employed in AI at Talkdesk. He also makes
